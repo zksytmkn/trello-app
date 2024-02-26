b0b12ec8546330ca4dbc14a4e75ca5c9bdbdd4c6
nakamoto <zksytmkn4869@gmail.com>
2024-02-27 00:56:49 +0900

    ✨

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/*.css"],
+  tailwind: true
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
